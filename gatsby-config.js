8624639921b2c551c47ac92d29a2457cfcfcb4d5
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-11 21:32:32 +0530

    build

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+    pathPrefix: '/',
+    siteMetadata: require('./site-metadata.json'),
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-source-data`,
+	`gatsby-plugin-catch-links`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-stackbit-static-sass`,
+            options: {
+                inputFile: `${__dirname}/src/sass/main.scss`,
+                outputFile: `${__dirname}/public/assets/css/main.css`
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [`gatsby-remark-component`]
+            }
+        },
+        {
+            resolve: `gatsby-remark-page-creator`,
+            options: {
+                
+            }
+        },
+        {
+            resolve: `@stackbit/gatsby-plugin-menus`,
+            options: {
+                sourceUrlPath: `fields.url`,
+                pageContextProperty: `menus`,
+                menus: require('./src/data/menus.json'),
+            },
+        },
+	{
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#F7F9FB`,
+        theme_color: `#006400`,
+        display: `standalone`,
+      },
+    },
+	`gatsby-plugin-offline`,
+
+    ]
+};
